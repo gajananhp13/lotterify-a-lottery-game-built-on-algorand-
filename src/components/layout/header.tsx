162bf15ecf1a8be6244dfa5c7a71c08a7ce83911
Gajanan Patange <goatgajanan13@gmail.com>
2025-10-13 11:38:06 +0000

    change the logo of lotterify app make sure it resembles to the lottery t

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import Link from "next/link";
@@ -11,25 +12,7 @@ import { cn } from "@/lib/utils";
 
 const Logo = () => (
     <Link href="/" className="flex items-center gap-2" aria-label="Lotterify Home">
-        <svg
-            width="32"
-            height="32"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            className="text-primary"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="M4 9h16" />
-            <path d="M4 15h16" />
-            <path d="M9.5 9v6" />
-            <path d="M14.5 9v6" />
-            <path d="M2 12a2 2 0 0 0 2-2V6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v4a2 2 0 0 0 2 2" />
-            <path d="M2 12a2 2 0 0 1 2 2v4a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-4a2 2 0 0 1 2-2" />
-        </svg>
+        <Ticket className="h-8 w-8 text-primary" />
         <span className="text-2xl font-headline font-bold">Lotterify</span>
     </Link>
 );
