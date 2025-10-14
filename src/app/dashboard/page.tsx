99e5265543bb2dffbf88d5ccdf2a62ce9fee9d56
Gajanan Patange <goatgajanan13@gmail.com>
2025-10-14 09:52:37 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -22,7 +22,7 @@ const RoundsIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-6 w-6">
         <path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z"/>
         <path d="m10 8 4 4-4 4"/>
-    </p>
+    </svg>
 )
 
 export default function DashboardPage() {
